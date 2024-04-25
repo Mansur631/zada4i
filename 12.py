b2ac50090aca775618b11b12f9d940fe437e62c7
mansu <stgyyavsesmogyy@gmail.com>
2024-04-25 18:34:46 +0300

    Задача #12

--- 12.py
+++ 12.py
@@ -1,13 +1,18 @@
-def calculate_tax():
-    amount = 95
-    nalog = 16
-    amount_nalog = (amount * nalog)/100
-    total_with_tax(amount, nalog, amount_nalog)
+amount = int(input("Цена товара: "))
+nalog = int(input("Сумма налога: "))
 
 
-def total_with_tax(some_amount, some_nalog, some_amount_nalog):
-    total = some_amount + some_amount_nalog
-    print(total)
+def calculate_tax(amount, nalog):
+    amount_nalog = (amount * nalog) / 100
+    return amount_nalog
 
 
-calculate_tax()
+nalog_amount = calculate_tax(amount, nalog)
+
+
+def total_with_tax(nalog_amount):
+    summa = amount + nalog_amount
+    return summa
+
+
+print(total_with_tax(nalog_amount))
