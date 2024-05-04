9fff9e6d2c41da2346f12bb3938dbae90300af85
mansu <stgyyavsesmogyy@gmail.com>
2024-05-04 13:59:13 +0300

    Задача #16 Сравнение возраста и возвращение списка кому больше 18 лет

--- 16.py
+++ 16.py
@@ -9,7 +9,20 @@ def read_from_file(file):
     return my_list
 
 
-print(read_from_file(file))
+def is_adult(age):
+    return int(age) >= 18
 
+
+def filter_adults(my_list):
+    new_my_list = []
+    for elements in my_list[1:]:
+        if is_adult(elements[1]):
+            new_my_list.append(elements)
+    return new_my_list
+
+
+my_list = read_from_file(file)
+big_age = filter_adults(my_list)
+print(big_age)
 
 file.close()
