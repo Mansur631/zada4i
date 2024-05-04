baa101713b7607d18711b6166419c51866f96f0e
mansu <stgyyavsesmogyy@gmail.com>
2024-05-04 15:43:55 +0300

    Задача #16 Добавил комментарии у функциям

--- 16.py
+++ 16.py
@@ -1,6 +1,6 @@
 file = open('students.txt', 'r')
 
-
+# 1. Чтение файла и преобразование данных
 def read_from_file(file):
     files = file.read()
     my_list = []
@@ -8,7 +8,7 @@ def read_from_file(file):
         my_list.append(tuple(row.split(',')))
     return my_list
 
-
+# 2. Определение совершеннолетия
 def is_adult(age):
     return int(age) >= 18
 
@@ -20,7 +20,7 @@ def filter_adults(my_list_age):
             new_my_list_age.append(elements)
     return new_my_list_age
 
-
+# 3. Подсчет студентов с оценкой выше 80
 def count_high_grades(my_list_grade):
     new_list_grade = []
     for student in my_list_grade[1:]:
@@ -28,7 +28,7 @@ def count_high_grades(my_list_grade):
             new_list_grade.append(student)
     return len(new_list_grade)
 
-
+# 4. Студенты с оценкой выше среднего
 def average_grade(my_list_students):
     grade_list = []
     for grade in my_list_students[1:]:
