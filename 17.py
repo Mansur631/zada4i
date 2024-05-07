41ba5d5b8eb9b83f77b7a39b131f0e0598752c02
mansu <stgyyavsesmogyy@gmail.com>
2024-05-07 13:29:33 +0300

    Задача #17 Готовая задача

--- 17.py
+++ 17.py
@@ -10,46 +10,21 @@ def open_from_file(file):
 
 
 def my_dictionary(sale_dict):
-    num1 = 0
-    num2 = 0
-    num3 = 0
     my_dict = {}
     for element in sale_dict:
-        my_dict[element[0]] = int(element[1])
         if my_dict.get(element[0]):
             my_dict[element[0]] += int(element[1])
+        else:
+            my_dict[element[0]] = int(element[1])
+    return my_dict
 
 
-# def sum_all_vse():
-#     pass
-#
-#
-# def summa_sale(all_sale_dict):
-
-
-# def summa_sale(all_sale_dict):
-#     num1 = 0
-#     num2 = 0
-#     num3 = 0
-#     my_dict = []
-#     for elements in all_sale_dict:
-#         if elements[0] == 'яблоки':
-#             # my_dict.append(num1 += int(elements[1]))
-#             num1 += int(elements[1])
-#             # my_dict.append(elements[0])
-#             # my_dict.append(num1)
-#         elif elements[0] == 'апельсины':
-#             num2 += int(elements[1])
-#             # my_dict.append(int(elements[1]))
-#         elif elements[0] == 'бананы':
-#             num3 += int(elements[1])
-#             # my_dict.append(int(elements[1]))
-#     return my_dict
+def from_dict_on_list(dict_of_list):
+    return list(dict_of_list.items())
 
 
 my_list = open_from_file(file)
-# my_fruits_dict = my_dictionary(my_list)
 my_dict = my_dictionary(my_list)
-# sales = summa_sale(my_list)
-
+my_new_list = from_dict_on_list(my_dict)
+print(my_new_list)
 file.close()
